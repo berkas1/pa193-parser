afb41a010331b3040f70fb0653fd1ec8226f1130
berkas1 <berka@berkasimon.com>
2021-03-25 19:09:03 +0100

    dataset tests now behave correctly

--- tests/dataset/test.sh
+++ tests/dataset/test.sh
@@ -13,7 +13,7 @@ echo "Parsed files:" $numberOfFiles
 for file in dataset/*.txt; do
 	newname=${file%.*}
 	
-	res=$(python3 output_compare.py .parser_output/$newname.json $newname.json 2> /dev/null)
+	res=$(python3 output_compare.py $newname.json .parser_output/$newname.json 2> /dev/null)
 	echo $newname".json => " $res
 	numberOfFiles=$(($numberOfFiles+1))
 done
