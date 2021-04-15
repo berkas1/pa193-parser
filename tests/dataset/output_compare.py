eeaf55c163b7591df89bc282bec1832469c0d7e3
berkas1 <berka@berkasimon.com>
2021-04-15 23:12:38 +0200

    fix output_compare.py testing file

--- tests/dataset/output_compare.py
+++ tests/dataset/output_compare.py
@@ -14,6 +14,9 @@ def check_title(actual, expected):
             return 20
         return 0
 
+    if not "title" in expected:
+        return 0
+
     similarity = SequenceMatcher(None, actual["title"], expected["title"]).ratio()
     return 20 * similarity
 
@@ -21,6 +24,9 @@ def check_versions(actual, expected):
     if not "versions" in actual:
         if not "versions" in expected:
             return 20
+        return 0
+
+    if not "versions" in expected:
         return 0
 
     actual = actual["versions"]
@@ -51,13 +57,16 @@ def check_versions(actual, expected):
     return 20 * score / max_score
 
 def check_toc(actual, expected):
-    if not "table_of_content" in actual:
-        if not "table_of_content" in expected:
+    if not "table_of_contents" in actual:
+        if not "table_of_contents" in expected:
             return 20
         return 0
 
-    actual = list(filter(lambda x: len(x) == 3, actual["table_of_content"]))
-    expected = expected["table_of_content"]
+    if not "table_of_contents" in expected:
+        return 0
+
+    actual = list(filter(lambda x: len(x) == 3, actual["table_of_contents"]))
+    expected = expected["table_of_contents"]
 
     if len(expected) == 0:
         if len(actual) == 0:
@@ -89,6 +98,9 @@ def check_revisions(actual, expected):
     if not "revisions" in actual:
         if not "revisions" in expected:
             return 20
+        return 0
+
+    if not "revisions" in expected:
         return 0
 
     actual = list(filter(lambda x: set(x) == {"version", "date", "description"}, actual["revisions"]))
@@ -126,6 +138,9 @@ def check_bibliography(actual, expected):
             return 20
         return 0
 
+    if not "bibliography" in expected:
+        return 0
+
     actual = actual["bibliography"]
     expected = expected["bibliography"]
 
@@ -147,8 +162,8 @@ def check_bibliography(actual, expected):
 
 
 def main():
-    actual = load_file(sys.argv[1])
-    expected = load_file(sys.argv[2])
+    expected = load_file(sys.argv[1])
+    actual = load_file(sys.argv[2])
 
     checks = (check_title, check_versions, check_toc, check_revisions, check_bibliography)
     points = 0
